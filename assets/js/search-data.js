ecaae4f2ea9d48abda8a6239409a0d6323f6f70c
lingxijin <jinlingxi07@gmail.com>
2025-09-20 11:45:58 +0000

    Deploying to gh-pages from @ lingxijin/lingxijin.github.io@250d53e8ac1fba85d70dc633a45e12519cb1793e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,18 +440,18 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
           section: "News",},{id: "news-thrilled-to-received-the-aied-2025-scholarshhip-award-1500",
           title: 'Thrilled to received the AIED 2025 Scholarshhip Award(~€1500)',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",},{id: "news-a-long-announcement-with-details",
+          title: 'A long announcement with details',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_2/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
