6431b63ecd9b63bbaaf3aef3e3210d88b13b3a69
lingxijin <jinlingxi07@gmail.com>
2025-09-20 11:55:36 +0000

    Deploying to gh-pages from @ lingxijin/lingxijin.github.io@ac6512d4c91aef136a6aa3ba74124aa9b1c197e5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,11 +440,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-thrilled-to-received-the-aied-2025-scholarshhip-award-1500",
-          title: 'Thrilled to received the AIED 2025 Scholarshhip Award(~€1500)',
+            },},{id: "news-thrilled-to-received-the-aied-2025-scholarship-award-1500",
+          title: 'Thrilled to received the AIED 2025 Scholarship Award(~€1500)',
           description: "",
           section: "News",},{id: "news-honored-to-receive-the-italian-government-scholarship-maeci-grant-9000",
-          title: 'Honored to receive the Italian Government Scholarship - **MAECI Grant** (€9000)',
+          title: 'Honored to receive the Italian Government Scholarship - MAECI Grant (€9000)',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
