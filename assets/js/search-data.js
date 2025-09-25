879e90fd1298179570fe59a02e476ab15c72fc16
lingxijin <jinlingxi07@gmail.com>
2025-09-25 08:58:59 +0000

    Deploying to gh-pages from @ lingxijin/lingxijin.github.io@e8029df8ff101d4be576e92c2b3262ae1e3cc34d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -104,9 +104,9 @@ ninja.data = [{
           description: "a project with no image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-design-and-evaluation-of-recommender-systems-for-educational-artificial-intelligence-platforms-focusing-on-preference-consistent-and-preference-inconsistent-recommendation-methods",
-          title: 'Design and Evaluation of Recommender Systems for Educational Artificial Intelligence Platforms: Focusing on...',
-          description: "Recommender system",
+            },},{id: "projects-design-and-evaluation-of-recommender-systems-for-educational-artificial-intelligence-platforms",
+          title: 'Design and Evaluation of Recommender Systems for Educational Artificial Intelligence Platforms',
+          description: "Design and Evaluation of Recommender systems for Educational Artificial Intelligence Platforms: Focusing on Preference-consistent and Preference-inconsistent Recommendation Methods",
           section: "Projects",handler: () => {
               window.location.href = "/projects/7_project/";
             },},{id: "projects-project-8",
