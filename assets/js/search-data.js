1509c41dd0c0f6de61315f4d3b9272ba34be9a08
lingxijin <jinlingxi07@gmail.com>
2025-11-01 13:03:12 +0000

    Deploying to gh-pages from @ lingxijin/lingxijin.github.io@3dddb8358c0175895a5265ebc26f5e890e5a3325 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,7 +24,7 @@ ninja.data = [{
             window.location.href = "/news/";
           },
         },{id: "nav-services",
-          title: "services",
+          title: "Services",
           description: "Academic Services",
           section: "Navigation",
           handler: () => {
